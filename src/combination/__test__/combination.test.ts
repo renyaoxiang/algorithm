a100156836901b429a7cb3010500a0da82038cee
renyaoxiang <renyaoxiang@aliyun.com>
2022-03-04 00:38:17 +0800

    update

--- src/combination/__test__/combination.test.ts
+++ src/combination/__test__/combination.test.ts
@@ -1,7 +1,7 @@
 import chai from "chai";
-import chaiArrays from "chai-arrays" 
+import chaiArrays from "chai-arrays";
 chai.use(chaiArrays);
- 
+
 chai.use(chaiArrays);
 const expect = chai.expect;
 import dataList from "./data";
@@ -9,7 +9,7 @@ import combination from "../combination";
 describe("combination", function () {
   it("combination should true", function () {
     for (let data of dataList) {
-      const output = combination(data.input); 
+      const output = combination(data.input);
       expect(output.length).to.be.equal(data.output.length);
       data.output.forEach((it: any, index: string | number) => {
         expect(output[index]).to.be.equalTo(it);
--- src/combination/combination.ts
+++ src/combination/combination.ts
@@ -13,9 +13,59 @@ export default function (input) {
       if (input.length === 0) {
         // do nothing
       } else if (input.length === 1) {
-        output.push(input[0]);
+        output.push([input[0]]);
       } else {
-        
+        actions.unshift(
+          makePayload("toSplitForPartialCombination", { input, output })
+        );
+      }
+    } else if (type === "toSplitForPartialCombination") {
+      const { input, output } = payload;
+      for (let i = input.length; i > 0; i--) {
+        actions.unshift(
+          makePayload("toPartialCombination", { input, length: i, output })
+        );
+      }
+    } else if (type === "toPartialCombination") {
+      const { input, output, length } = payload;
+      const partResult = [];
+      const onFilled = (partResult) => {
+        output.push([...partResult]);
+      };
+      actions.unshift(
+        makePayload("partialCombination", {
+          partResult,
+          length,
+          input,
+          current: 0,
+          onFilled,
+        })
+      );
+    } else if (type === "partialCombination") {
+      const { partResult, length, input, current, onFilled } = payload;
+      if (partResult.length === length) {
+        onFilled(partResult);
+      } else if (partResult.length < length) {
+        if (current < input.length) {
+          actions.unshift(
+            makePayload("partialCombination", {
+              partResult: [...partResult],
+              length,
+              input,
+              current: current + 1,
+              onFilled,
+            })
+          );
+          actions.unshift(
+            makePayload("partialCombination", {
+              partResult: [...partResult, input[current]],
+              length,
+              input,
+              current: current + 1,
+              onFilled,
+            })
+          );
+        }
       }
     }
   }
